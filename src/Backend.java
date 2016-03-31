b89b5b66a32512315f273a816239a0fe749e9b77
Michael Sime <ms255@st-andrews.ac.uk>
2016-03-31 22:14:44 +0100

    Basic PubSub Example. Must Test

--- src/Backend.java
+++ src/Backend.java
@@ -1,56 +1,39 @@
 import javax.jms.*;
-import javax.naming.*;
-import java.util.Properties;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
-public class Backend implements MessageListener {
+public class Backend  {
 
-    private TopicSession pubSession;
-    private TopicSession subSession;
-    private TopicPublisher publisher;
-    private TopicConnection connection;
-    private String username;
+    static CountDown done = new CountDown(1);
+    TopicConnection conn = null;
+    TopicSession session = null;
+    Topic topic = null;
 
+    public static class ExListener implements MessageListener
+    {
+        public void onMessage(Message msg)
+        {
+            done.release();
+            TextMessage tm = (TextMessage) msg;
+            try {
+                System.out.println("onMessage, recv text=" + tm.getText());
+            } catch(Throwable t) {
+                t.printStackTrace();
+            }
+        }
+    }
 
-    public Backend(String topicName, String username, String password) {
-        // Obtain a JNDI connection
-        Properties env = new Properties();
-        // Specify the JNDI properties specific to the vendor
+    public void setupPubSub(){
+        InitialContext iniCtx = null;
         try {
-            InitialContext jndi = new InitialContext(env);
-            // Look up a JMS connection factory
-            TopicConnectionFactory topicConnectionFactory = (TopicConnectionFactory)
-                    jndi.lookup("TopicConnectionFactory");
-
-            // Create a JMS Connection
-            TopicConnection connection =
-                    topicConnectionFactory.createTopicConnection();
-
-            // Create two JMS session objects
-            TopicSession pubSession =
-                    connection.createTopicSession(false,
-                            Session.AUTO_ACKNOWLEDGE);
-            TopicSession subSession =
-                    connection.createTopicSession(false,
-                            Session.AUTO_ACKNOWLEDGE);
-
-            // Look up a JMS topic
-            Topic messageTopic = (Topic) jndi.lookup(topicName);
-
-            TopicPublisher publisher =
-                    pubSession.createPublisher(messageTopic);
-            TopicSubscriber subscriber =
-                    subSession.createSubscriber(messageTopic);
-
-            // Set a JMS message listener
-            subscriber.setMessageListener(this);
-
-            // Intialize the Chat application
-            set(connection, pubSession, subSession, publisher, username);
-
-            // Start the JMS connection; allows messages to be delivered
-            connection.start();
-
-
+            iniCtx = new InitialContext();
+            Object tmp = iniCtx.lookup("ConnectionFactory");
+            TopicConnectionFactory tcf = (TopicConnectionFactory) tmp;
+            conn = tcf.createTopicConnection();
+            topic = (Topic) iniCtx.lookup("topic/testTopic");
+            session = conn.createTopicSession(false,
+                    TopicSession.AUTO_ACKNOWLEDGE);
+            conn.start();
         } catch (NamingException e) {
             e.printStackTrace();
         } catch (JMSException e) {
@@ -58,27 +41,50 @@ public class Backend implements MessageListener {
         }
     }
 
-    /* Initialize the instance variables */
-    public void set(TopicConnection con, TopicSession pubSess,
-                    TopicSession subSess, TopicPublisher pub,
-                    String username) {
-        this.connection = con;
-        this.pubSession = pubSess;
-        this.subSession = subSess;
-        this.publisher = pub;
-        this.username = username;
+    public void sendRecvAsync(String text){
+        System.out.println("Begin sendRecvAsync");
+        // Setup the PubSub connection, session
+        setupPubSub();
+        // Set the async listener
+
+        TopicSubscriber recv = null;
+        try {
+            recv = session.createSubscriber(topic);
+            recv.setMessageListener(new ExListener());
+            // Send a text msg
+            TopicPublisher send = session.createPublisher(topic);
+            TextMessage tm = session.createTextMessage(text);
+            send.publish(tm);
+            System.out.println("sendRecvAsync, sent text=" + tm.getText());
+            send.close();
+            System.out.println("End sendRecvAsync");
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
 
-    /* Close the JMS connection */
-    public void close() throws JMSException {
-        connection.close();
+    public void stop() {
+        try {
+            conn.stop();
+            session.close();
+            conn.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void main(String[] args) {
-    }
-
-    @Override
-    public void onMessage(Message message) {
-
+    public static void main(String args[]) {
+        System.out.println("Begin TopicSendRecvClient, now=" +
+                System.currentTimeMillis());
+        Backend client = new Backend();
+        client.sendRecvAsync("A text msg, now="+System.currentTimeMillis());
+        try {
+            client.done.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        client.stop();
+        System.out.println("End TopicSendRecvClient");
+        System.exit(0);
     }
 }
