6310803a1c83b59f2da81d8498b804498e161665
IlyaGetReady <ilyagetready@gmail.com>
2016-04-11 00:01:28 +0100

    TODOs added

--- PUBSUB_SYSTEM/SERVER/server.py
+++ PUBSUB_SYSTEM/SERVER/server.py
@@ -14,8 +14,9 @@ from collections import defaultdict
 from BaseHTTPServer import HTTPServer
 
 subscribers = {} # user_token -> client
-publishers = defaultdict(list) # service_token -> tags
+publishers = {} # service_token -> client
 
+publishers_tags = defaultdict(list) # service_token -> tags
 interest = defaultdict(list) # user_token -> tags
 
 def subscribe(username, password, port):
@@ -25,17 +26,18 @@ def subscribe(username, password, port):
     return {"subscriber_id": token}
 
 def subscribe_to_tags(token, tags):
-    interest[token] += tags
+    interest[token] += tags # TODO: check if that works
     return {"errorcode": globalconf.SUCCESS_CODE}
 
 def register_publisher(service_name, port, tags):
     token = utils.generate_token(service_name, service_name, port)
     client = SoapClient(location=globalconf.hostname % str(port), action=globalconf.hostname % str(port),
                         soap_ns="soap", trace=True, ns=True)
-    publishers[token] += tags
+    publishers_tags[token] += tags #TODO: check if this works
     return {"errorcode": globalconf.SUCCESS_CODE}
 
 def publish(service_token, message):
+    #TODO: add publishing capability based on a set of tags
     pass
 
 dispatcher = SoapDispatcher(
@@ -47,8 +49,8 @@ dispatcher = SoapDispatcher(
     ns=True)
 
 dispatcher.register_function('subscribe', subscribe,
-                                  returns={"subscriber_id": str},
-                                  args={"username": str, "password":str, "port": str})
+                              returns={"subscriber_id": str},
+                              args={"username": str, "password":str, "port": str})
 
 dispatcher.register_function('subscribe_to_tags', subscribe_to_tags,
                              returns={"errorcode": int},
